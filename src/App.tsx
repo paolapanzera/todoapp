5d824623fa3da0964899146de66976c341c1805b
Paola Panzera <paola.panzera@avanade.com>
2021-07-02 09:14:31 +0200

    adding folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import './App.scss';
-import { Active } from './pages/Active';
-import { All } from './pages/All';
-import { Completed } from './pages/Completed';
+import { Active } from './components/Active/Active';
+import { All } from './components/All';
+import { Completed } from './components/Completed';
 
 const App: React.FC  = () => {
   const [tab, setTab] = useState<string>('All')
@@ -10,10 +10,11 @@ const App: React.FC  = () => {
   return (
     <div className="app">
       <h1 className="title">#todo</h1>
-      <button className="btnTab" onClick={() => setTab('All')}>All</button>
-      <button className="btnTab" onClick={() => setTab('Active')}>Active</button>
-      <button className="btnTab" onClick={() => setTab('Completed')}>Completed</button>
-
+      <div className="containerBtnTabs">
+        <button className="btnTab" onClick={() => setTab('All')}>All</button>
+        <button className="btnTab" onClick={() => setTab('Active')}>Active</button>
+        <button className="btnTab" onClick={() => setTab('Completed')}>Completed</button>
+      </div>
       { tab === 'All' && <All /> }
       { tab === 'Active' && <Active /> }
       { tab === 'Completed' && <Completed /> }
